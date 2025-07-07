5ccd4c33f5300a038b5d9a871a07f58170e52b6e
PhilNel <phil15634280@gmail.com>
2025-07-07 06:54:59 +0200

    fix: Remove unused variable on route53 module

--- src/modules/route53/variables.tf
+++ src/modules/route53/variables.tf
@@ -2,8 +2,3 @@ variable "domain_name" {
   description = "Root domain name (e.g., boardgamewarlock.com)"
   type        = string
 }
-
-variable "environment" {
-  description = "Environment name (dev, staging, prod)"
-  type        = string
-} 
